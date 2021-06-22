85d31f44ccdea7268c1242fb2ed0174cc48ec2d9
celguar <celguar@gmail.com>
2021-06-22 03:34:44 +0300

    Crashfixes
    Fix crash in player relocation
    Fix crash in targeted movement generator log
    Crashfix attempt
    Temp fix for bg event crash
    
    Co-Authored-By: blitzi <289959+blitzi@users.noreply.github.com>

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -8545,7 +8545,7 @@ uint32 Unit::SpellDamageBonusTaken(Unit* caster, SpellEntry const* spellProto, u
     AuraList const& mOwnerTaken = GetAurasByType(SPELL_AURA_MOD_DAMAGE_FROM_CASTER);
     for (auto i : mOwnerTaken)
     {
-        if (i->GetCasterGuid() == caster->GetObjectGuid() && i->isAffectedOnSpell(spellProto))
+        if (caster && i->GetCasterGuid() == caster->GetObjectGuid() && i->isAffectedOnSpell(spellProto))
             TakenTotalMod *= (i->GetModifier()->m_amount + 100.0f) / 100.0f;
     }
 
@@ -13715,7 +13715,9 @@ void Unit::Uncharm(Unit* charmed, uint32 spellId)
             charmed->InterruptSpell(CURRENT_AUTOREPEAT_SPELL);
             charmed->InterruptSpell(CURRENT_MELEE_SPELL);
 
-            charmInfo->ResetCharmState();
+            if (charmInfo)
+                charmInfo->ResetCharmState();
+
             charmed->DeleteCharmInfo();
 
             charmed->RemoveUnattackableTargets();
@@ -13729,7 +13731,8 @@ void Unit::Uncharm(Unit* charmed, uint32 spellId)
             if (Unit* owner = charmedCreature->GetOwner())
                 charmed->setFaction(owner->GetFaction());
 
-            charmInfo->ResetCharmState();
+            if (charmInfo)
+                charmInfo->ResetCharmState();
 
             // as possessed is a pet we have to restore original charminfo so Pet::DeleteCharmInfo will take care of that
             charmed->DeleteCharmInfo();
@@ -13748,7 +13751,9 @@ void Unit::Uncharm(Unit* charmed, uint32 spellId)
         else
             charmedPlayer->setFactionForRace(charmedPlayer->getRace());
 
-        charmInfo->ResetCharmState();
+        if (charmInfo)
+            charmInfo->ResetCharmState();
+
         charmedPlayer->DeleteCharmInfo();
 
         charmed->GetMotionMaster()->Clear(false, true);
